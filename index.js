ad7b142e4f5fcbbc6fe459ae1897a4d33cf3a8d4
alexander sampson <a.sampson206@gmail.com>
2023-11-18 20:41:55 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.querySelector(".img1").addEventListener("click", clickTheLynel)
+function clickTheLynel(){alert("Trap Set, and sprung! I hope you like being BBQ!")}
+
+document.querySelector("body").addEventListener("keydown", dummyKey)
+function dummyKey(){
+    alert("Oh, did I say press 'F'? Surprise! Lynels hate everything! Breath too loud and it's over!")
+}
+
+document.querySelector(".img2").addEventListener("mouseout", hereIGoHoveringAgain)
+function hereIGoHoveringAgain(){ 
+alert("He took something from you, but you won't realize what it is until it's too late")
+}
+
