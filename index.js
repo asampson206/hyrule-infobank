936a4d87f0dbf0d74ab22dab5cece2310af14a8f
alexander sampson <a.sampson206@gmail.com>
2023-11-20 20:16:19 -0800

    first event listener start

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 document.querySelector(".img1").addEventListener("click", clickTheLynel)
-function clickTheLynel(){alert("Trap Set, and sprung! I hope you like being BBQ!")}
 
-document.querySelector("body").addEventListener("keydown", dummyKey)
-function dummyKey(){
-    alert("Oh, did I say press 'F'? Surprise! Lynels hate everything! Breath too loud and it's over!")
-}
-
-document.querySelector(".img2").addEventListener("mouseout", hereIGoHoveringAgain)
-function hereIGoHoveringAgain(){ 
-alert("He took something from you, but you won't realize what it is until it's too late")
-}
 
