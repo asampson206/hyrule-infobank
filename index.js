f4baf2e7ea315d8a0ca91e816c81fe900b9fd107
alexander sampson <a.sampson206@gmail.com>
2023-11-20 20:17:11 -0800

    2nd event listener start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.querySelector(".img1").addEventListener("click", clickTheLynel)
 function clickTheLynel(){alert("Trap Set, and sprung! I hope you like being BBQ!")}
+document.querySelector("body").addEventListener("keydown", dummyKey)
 
 
 
