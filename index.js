5c97aa05b5d6d5a2a94454326ec9f04a24fe89b3
alexander sampson <a.sampson206@gmail.com>
2023-11-24 19:56:37 -0800

    final eventlistener

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 document.querySelector(".img1").addEventListener("click", clickTheLynel)
 function clickTheLynel(){alert("Trap Set, and sprung! I hope you like being BBQ!")}
 
-document.querySelector("body").addEventListener("keydown", dummyKey)
-function dummyKey(){
+const doc = document.querySelector('html')
+doc.addEventListener("keydown", (event) => {
+    let event1 = event.key;
+    let changer = doc.style.backgroundColor = 'red'
+    if(event1 === 'f'){return changer}
+})
+
+doc.addEventListener("keydown", (event) => {
+    let event1 = event.key;
+    let alert2 = alert('Start the drumroll then click \'Ok\' to see your fate! And thanks again for putting your safety, in our hands!')
+    if(event1 === 'f'){return alert2}
     
-}
+})
 
 document.querySelector(".img2").addEventListener("mouseenter", function (){
 let image2 = document.querySelector(".img2")
