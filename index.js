135ddf25d44380e1706846ed1020ac3d5361e7d5
alexander sampson <a.sampson206@gmail.com>
2023-11-24 19:08:22 -0800

    adding 2nd event listener function

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ function clickTheLynel(){alert("Trap Set, and sprung! I hope you like being BBQ!
 
 document.querySelector("body").addEventListener("keydown", dummyKey)
 function dummyKey(){
-    alert("Oh, did I say press 'F'? Surprise! Lynels hate everything! Breath too loud and it's over!")
+    
 }
 
-document.querySelector(".img2").addEventListener("mouseout", hereIGoHoveringAgain)
-function hereIGoHoveringAgain(){ 
-    alert("He took something from you, but you won't realize what it is until it's too late")
-    }
+document.querySelector(".img2").addEventListener("mouseenter", function (){
+let image2 = document.querySelector(".img2")
+image2.title = 'If you see this, you should definitely run!'
+})
 
 
+    
 
